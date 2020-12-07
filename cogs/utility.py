bc66f5969db6b369524ac0f846a736010b92260c
Liam Bryant <grasshelmet@gmail.com>
2020-12-07 01:57:55 -0800

    Utility Cog with timezone command

---
+++ cogs/utility.py
@@ -0,0 +1,34 @@
+import discord,time,pytz
+from datetime import datetime,timezone
+from discord.ext import commands
+from checks import check_owner
+
+class Utility(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+        self.last_membner = None
+
+    @commands.command(aliases=['tz'])
+    @commands.group()
+    async def timezone(self,ctx,arg1=None):
+        frmt = '%I:%M %p %Z'
+        c_utc = datetime.now(pytz.timezone('UTC'))
+        if arg1 == None:
+            await ctx.channel.send('Current time is {}'.format(c_utc.strftime(frmt) ))
+        else:
+            abbrvs ={
+                    'PST':'America/Los_Angeles',
+                    'CST':'CST6CDT',
+                    'AEST':'Australia/Brisbane'
+                }
+            result = abbrvs.get(arg1,'NONE')
+            if result != 'NONE':
+                arg1 = result
+            tz = pytz.timezone(arg1)
+            ti = c_utc.astimezone(tz)
+            await ctx.channel.send('Current time is {}'.format(ti.strftime(frmt) ))
+
+
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
