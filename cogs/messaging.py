b33dfee3ebb7bbc826d225de494c7de3d239632f
Liam Bryant <grasshelmet@gmail.com>
2020-12-16 23:56:03 -0800

    Made portal feature WAAAAAY less buggy

--- cogs/messaging.py
+++ cogs/messaging.py
@@ -1,4 +1,5 @@
 import discord
+import typing
 from discord.ext import commands
 from bot_config.checks import check_owner
 
@@ -33,50 +34,58 @@ class Messaging(commands.Cog):
     #Set a channel to respond to
     @commands.group(invoke_without_command=True)
     @commands.check(check_owner)
-    async def portal(self,ctx,channel : discord.TextChannel):
-        print(self.port_pairs)
-        try:
-            if self.port_pairs[channel] != None:
-                await ctx.channel.send('Portal Closed in {0}'.format(channel))
-                self.port_pairs[channel] = None
-            else:
-                await ctx.channel.send('Portal Opened in {0}'.format(channel))
-                self.port_pairs[channel] = ctx.channel
-        except:
-            await ctx.channel.send('Portal Opened in {0}'.format(channel))
-            self.port_pairs[channel] = ctx.channel
-
-    @portal.command()
-    async def look(self,ctx,gid):
-        channel = self.bot.get_channel(int(gid))
+    async def portal(self,ctx,channel: typing.Union[discord.TextChannel, int]):
+        if type(channel) is int:
+            print('Hello:{}\n'.format(channel))
+            channel = self.bot.get_channel(channel)
         if channel == None:
             await ctx.channel.send('Failed to find channel')
-            return 
+            return
 
         try:
             if self.port_pairs[channel] != None:
                 await ctx.channel.send('Portal Closed in {0}'.format(channel))
-                self.port_pairs[channel] = None
+                self.port_pairs.pop(channel)
             else:
                 await ctx.channel.send('Portal Opened in {0}'.format(channel))
                 self.port_pairs[channel] = ctx.channel
         except:
             await ctx.channel.send('Portal Opened in {0}'.format(channel))
             self.port_pairs[channel] = ctx.channel
+        print(self.port_pairs)
 
+    @portal.command()
+    async def close(self,ctx,channel: typing.Union[discord.TextChannel, int]=None):
+        if type(channel) is int:
+            channel = self.bot.get_channel(channel)
+        if channel == None:
+            for key in self.port_pairs.keys():
+                if ctx.channel == self.port_pairs[key]:
+                    channel = key 
+                    break
+        try:
+            if self.port_pairs[channel] != None:
+                await ctx.channel.send('Portal Closed in {0}'.format(channel))
+                self.port_pairs.pop(channel)
+        except:
+            await ctx.channel.send('Portal Not There,so nothin happened')
+        
 
+    #clear all portals
     @portal.command()
     async def clear(self,ctx):
         self.port_pairs = {}
         await ctx.channel.send(self.port_pairs)
 
+    #see the current portal pairs data, is a little hard to read
     @portal.command()
     async def see(self,ctx):
         await ctx.channel.send(self.port_pairs)
 
     @commands.Cog.listener('on_message')
     async def port(self,message):
-        if message.content.startswith('!portal'):
+        prefix = await self.bot.get_prefix(message)
+        if message.content.startswith('{0}portal'.format(prefix[2])):
             return
         if message.author.bot:
             return
