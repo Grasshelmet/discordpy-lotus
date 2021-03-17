aab037b64eaa49ffcd35e8331e6938fef2aa98ae
Grasshelmet <50856706+Grasshelmet@users.noreply.github.com>
2021-03-16 21:34:03 -0700

    Refactored connect/disconnect, can almost play audio files

--- cogs/voice.py
+++ cogs/voice.py
@@ -1,5 +1,7 @@
-import discord,typing
+import discord,typing,time
 from discord.ext import commands
+
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 class Voice(commands.Cog):
     def __init__(self,bot):
@@ -7,34 +9,48 @@ class Voice(commands.Cog):
         self.last_member = None
         self.active_vcs = []
 
+    #connects bot to vc
     @commands.command(aliases=['ct','cnt'])
-    async def connect(self,ctx, channel: typing.Union[discord.VoiceChannel, int]):
-        if type(channel) is int:
-            channel = self.bot.get_channel(channel)
-        if channel == None:
-            await ctx.channel.send('Failed to find channel')
-            return
-        try:
-            self.active_vcs.append(await channel.connect())
-        except Exception as e:
-            await ctx.send("Failed to connect to {}\nError: {}".format(channel.mention,e))
+    async def connect(self,ctx):
+        channel = ctx.author.voice.channel
+        vc = discord.utils.get(self.bot.voice_clients,guild=ctx.guild)
 
+        if vc and vc.is_connected():
+            await vc.move_to(channel)
+        else:
+            try:
+                vc = await channel.connect()
+            except Exception as er:
+                await ctx.send("Failed to connect to {}\nError: {}".format(channel.mention,er))
+                return
+
+    #Disconnects bot from vc
     @commands.command(aliases=['dt','dsct'])
-    async def disconnect(self,ctx,channel: typing.Union[discord.VoiceChannel, int]):
-        if type(channel) is int:
-            channel = self.bot.get_channel(channel)
-        if channel == None:
-            await ctx.channel.send('Failed to find channel')
-            return
-          
-        activeCon = None
-        for con in self.active_vcs:
-            if con.channel == channel:
-                activeCon = con
-        if activeCon == None:
-            await ctx.channel.send("Couldn't find active connection")
-            return
-        await con.disconnect()
-        con.cleanup()
+    async def disconnect(self,ctx,):
+        channel = ctx.author.voice.channel
+        vc = discord.utils.get(self.bot.voice_clients,guild=ctx.guild)
+
+        await vc.disconnect()
+        vc.cleanup()
+
+    #plays an audio file at the specified location
+    @commands.command()
+    async def play(self,ctx,file):
+        channel = ctx.author.voice.channel
+        vc = discord.utils.get(self.bot.voice_clients,guild=ctx.guild)
+
+        if vc and vc.is_connected():
+            await vc.move_to(channel)
+        else:
+            try:
+                vc = await channel.connect()
+            except Exception as er:
+                await ctx.send("Failed to connect to {}\nError: {}".format(channel.mention,er))
+                return
+
+
+        vc.play(discord.FFmpegPCMAudio(file,**FFMPEG_OPTIONS),after=lambda e: print("Failed to play",e))
+        vc.is_playing()
+        
 def setup(bot):
     bot.add_cog(Voice(bot))
